1465af2d20662b3bcbecba34015efdcf4417c89e
AbyssHate <rip8611@gmail.com>
2023-08-05 23:17:28 +0300

    1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,103 @@
+using Colyseus.Schema;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private EnemyCharacter _charecter;
+    [SerializeField] private EnemyGun _gun;
+    private List<float> _receiveTimeInterval = new List<float> { 0, 0, 0, 0, 0 };
+    private float AveregeInterval
+    {
+        get
+        {
+            int receiveTimeIntervalCount = _receiveTimeInterval.Count;
+            float summ = 0;
+            for(int i = 0; i < receiveTimeIntervalCount; i++)
+            {
+                summ += _receiveTimeInterval[i];
+            }
+            return summ / receiveTimeIntervalCount;
+        }
+    }
+    private float _lastReceiveTime = 0f;
+    private Player _player;
+
+    public void Init(Player player)
+    {
+        _player = player;
+        _charecter.SetSpeed(player.speed);
+        player.OnChange += OnChange;
+    }
+
+    public void Shoot(in ShootInfo info)
+    {
+        Vector3 position = new Vector3(info.pX, info.pY, info.pZ);
+        Vector3 velocity = new Vector3(info.dX, info.dY, info.dZ);
+
+        _gun.Shoot(position, velocity);
+    }
+
+    public void Destroy()
+    {
+        _player.OnChange -= OnChange;
+        Destroy(gameObject);
+    }
+
+    private void SaveReceiveTime()
+    {
+        float interaval = Time.time - _lastReceiveTime;
+        _lastReceiveTime = Time.time;
+
+        _receiveTimeInterval.Add(interaval);
+        _receiveTimeInterval.Remove(0);
+    }
+
+    internal void OnChange(List<DataChange> changes)
+    {
+        SaveReceiveTime();
+
+        Vector3 position = _charecter.targetPosition;
+        Vector3 velocity = _charecter.velocity;
+
+        foreach (var dataChange in changes)
+        {
+            switch (dataChange.Field)
+            {
+                case "pX":
+                    position.x = (float)dataChange.Value;
+                    break;
+                case "pY":
+                    position.y = (float)dataChange.Value;
+                    break;
+                case "pZ":
+                    position.z = (float)dataChange.Value;
+                    break;
+                case "vX":
+                    velocity.x = (float)dataChange.Value;
+                    break;
+                case "vY":
+                    velocity.y = (float)dataChange.Value;
+                    break;
+                case "vZ":
+                    velocity.z = (float)dataChange.Value;
+                    break;
+                case "rX":
+                    _charecter.SetRotateX((float)dataChange.Value);
+                    break;
+                case "rY":
+                    _charecter.SetRotateY((float)dataChange.Value);
+                    break;
+                default:
+                    Debug.LogWarning("Не обрабатывается изменение поля" +  dataChange.Field);
+                    break;
+
+            }
+        }
+
+        _charecter.SetMovement(position, velocity, AveregeInterval);
+    }
+}
